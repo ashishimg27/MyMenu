4cdf810e222791a9dded06c31c4a688df90b6468
ashishimg27 <95354816+ashishimg27@users.noreply.github.com>
2021-12-06 10:41:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import Axios from "axios";
+import { useState } from "react";
+import "./app.css";
+import RecipeTile from "./components/recipe-tile";
+
+function App() {
+  const [query, setquery] = useState("");
+  const [recipes, setrecipes] = useState([]);
+
+  const YOUR_APP_ID = `8cc7e840`;
+  const YOUR_APP_KEY = "d7a42b9b13a0c96e4000127f6b57b8ba";
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
+
+  const getRecipeInfo = async () => {
+    var result = await Axios.get(url);
+    setrecipes(result.data.hits);
+    console.log(result.data.hits);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getRecipeInfo();
+  };
+
+  return (
+    <div className="app">
+      <h1 onClick={getRecipeInfo}>My Menu 🍔</h1>
+      <form className="app__searchForm" onSubmit={onSubmit}>
+        <input
+          className="app__input"
+          type="text"
+          placeholder="enter ingridient"
+          autoComplete="Off"
+          value={query}
+          onChange={(e) => setquery(e.target.value)}
+        />
+        <input className="app__submit" type="submit" value="Search" />
+      </form>
+
+      <div className="app__recipes">
+        {recipes !== [] &&
+          recipes.map((recipe) => {
+            return <RecipeTile recipe={recipe} />;
+          })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
